e09df67a437a16788f288ed866ad24ac26d0ac98
michalskibinski109 <michalskibinski109@gmail.com>
2022-07-07 15:20:09 +0200

    one more test

--- test_engine.py
+++ test_engine.py
@@ -36,8 +36,20 @@ class TestChessEngine:
         # inbalance material
         plus_24_pos = Board(
             'rnb1k2B/pppp1p1p/6p1/4P3/8/8/PPP1PPPP/RN1QKBNR b KQq - 0 6')
-        assert (c.pieces_placement_eval(plus_2_pos)> .2)
+        assert (c.pieces_placement_eval(plus_2_pos) > .2)
         assert (-.2 > c.pieces_placement_eval(minus_2_pos))
-        assert(c.pieces_placement_eval(plus_24_pos)> 20)
+        assert(c.pieces_placement_eval(plus_24_pos) > 20)
         del c
 
+    def test_engine_method(self):
+        c = ChessEngine(1, Board())
+        w_mate_in_1 = Board(
+            'rnbqkbnr/1pp2ppp/p2p4/4p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR w KQkq - 0 4')
+        b_mate_in_1 = Board('8/K1k5/1r6/8/8/8/8/8 w - - 0 1')
+        b_mate_in_2 = Board('8/K7/2k5/8/8/1r6/8/8 w - - 0 1')
+        w_mate_in_3 = Board('k7/8/8/1RK5/8/8/8/8 w - - 0 1')
+        assert c.engine(w_mate_in_1, 0) == 100
+        assert c.engine(b_mate_in_1, 1) == -100
+        assert c.engine(b_mate_in_2, 3) == -100
+        assert c.engine(w_mate_in_3, 4) == 100
+        del c
